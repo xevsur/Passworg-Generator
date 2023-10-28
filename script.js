1f473be37a1ad4860934ab56aaefcd9fd9bafeb4
Gode gogochuri <godegogocha@gmail.com>
2023-10-28 19:24:28 +0400

    flex,sizing and fuction update

--- script.js
+++ script.js
@@ -8,125 +8,157 @@ let strengthBar1 = document.querySelector(".bar1");
 let strengthBar2 = document.querySelector(".bar2");
 let strengthBar3 = document.querySelector(".bar3");
 let strengthBar4 = document.querySelector(".bar4");
-let strengthTxt = document.querySelector(".strengthText");
+let strengthTxt = document.querySelector(".strengthtext");
 
-slider.addEventListener("input", function () {
-    value = (slider.value / slider.max) * 20;
-    range.innerHTML = value;
-    slider.style.background =
-      "linear-gradient(to right, #A4FFAF 0%, #A4FFAF " +
-      value * 5 +
-      "%, #18171F " +
-      value * 5 +
-      "%, #18171F 100%)";
-    console.log(value);
+
+
+
+function generate() {
+    var passLength = document.getElementById("passLength").value;
+  
+   
+    var includeUpper = document.getElementById("upperCheckbox").checked;
+    var includeLower = document.querySelector(".checkLower").checked;
+    var includeNums = document.querySelector(".checkNums").checked;
+    var includeSymbols = document.querySelector(".checkSymbol").checked;
+    
+    if (!includeUpper && !includeLower && !includeNums && !includeSymbols) {
+        document.querySelector(".passwordCont").textContent = "Choose from Options...";
+        return;
+      }
+
+    var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+    var numChars = "0123456789";
+    var symbolChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+    var charSet = "";
+    if (includeUpper) {
+      charSet += upperChars;
+    }
+    if (includeLower) {
+      charSet += lowerChars;
+    }
+    if (includeNums) {
+      charSet += numChars;
+    }
+    if (includeSymbols) {
+      charSet += symbolChars;
+    }
+  
+   
+   
+    var password = "";
+    for (var i = 0; i < passLength; i++) {
+      var randomIndex = Math.floor(Math.random() * charSet.length);
+      password += charSet[randomIndex];
+    }
+   
+
+    document.querySelector(".passwordCont").textContent = password;
+   
+
+    var strength = calculatePasswordStrength(password);
+
+    var length = password.length;
+  if (length > 0)
+    {if (strength < 25) {
+    strengthBar1.style.backgroundColor = "#F64A4A";
+    strengthBar1.style.border = "2px solid #F64A4A";
+    strengthBar2.style.backgroundColor = "#18171F";
+    strengthBar2.style.border = "2px solid #E6E5EA";
+    strengthBar3.style.backgroundColor = "#18171F";
+    strengthBar3.style.border = "2px solid #E6E5EA";
+    strengthBar4.style.backgroundColor = "#18171F";
+    strengthBar4.style.border = "2px solid #E6E5EA";
+  } else if (strength < 50) {
+    strengthBar1.style.backgroundColor = "#FB7C58";
+    strengthBar1.style.border = "2px solid #FB7C58";
+    strengthBar2.style.backgroundColor = "#FB7C58";
+    strengthBar2.style.border = "2px solid #FB7C58";
+    strengthBar3.style.backgroundColor = "#18171F";
+    strengthBar3.style.border = "2px solid #E6E5EA";
+    strengthBar4.style.backgroundColor = "#18171F";
+    strengthBar4.style.border = "2px solid #E6E5EA";
+  } else if (strength < 75) {
+    strengthBar1.style.backgroundColor = "#F8CD65";
+    strengthBar1.style.border = "2px solid #F8CD65";
+    strengthBar2.style.backgroundColor = "#F8CD65";
+    strengthBar2.style.border = "2px solid #F8CD65";
+    strengthBar3.style.backgroundColor = "#F8CD65";
+    strengthBar3.style.border = "2px solid #F8CD65";
+    strengthBar4.style.backgroundColor = "#18171F";
+    strengthBar4.style.border = "2px solid #E6E5EA";
+  } else {
+    strengthBar1.style.backgroundColor = "#A4FFAF";
+    strengthBar1.style.border = "2px solid #A4FFAF";
+    strengthBar2.style.backgroundColor = "#A4FFAF";
+    strengthBar2.style.border = "2px solid #A4FFAF";
+    strengthBar3.style.backgroundColor = "#A4FFAF";
+    strengthBar3.style.border = "2px solid #A4FFAF";
+    strengthBar4.style.backgroundColor = "#A4FFAF";
+    strengthBar4.style.border = "2px solid #A4FFAF";
+  };
+
+
+  if (strength < 25) {
+    strengthTxt.textContent = "Weak";
+  } else if (strength < 50) {
+    strengthTxt.textContent = "Moderate";
+  } else if (strength < 75) {
+    strengthTxt.textContent = "Strong";
+  } else if (strength > 75){
+    strengthTxt.textContent = "Very Strong";
+  }else{
+    strengthTxt.textContent = "";
+  }
+};
+
+  
+
+};
+
+slider.addEventListener("input", function() {
+    range.textContent = this.value;
   });
-
-  function copied() {
-    copy.style.display = "flex";
-    let textToCopy = password.textContent;
-    let textarea = document.createElement("textarea");
-    textarea.value = textToCopy;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textarea);
-  }
-
-  function generate() {
-    let checkNum = 0;
-    copy.style.display = "none";
+      function calculatePasswordStrength(password) {
+      var strength = 0;
+      var length = password.length;
+      var upperCase = /[A-Z]/g;
+      var lowerCase = /[a-z]/g;
+      var numbers = /[0-9]/g;
+      var symbols = /[!@#$%^&*()_+~`|}{[\]:;?><,./-=]/g;
   
-    check.forEach(function (checkbox) {
-      if (checkbox.checked) {
-        checkNum++;
+      if (length >= 8) {
+          strength += 25;
       }
-    });
-
-    check.forEach(function (box) {
-      box.addEventListener('change', function () {
-        let value = document.getElementById('passLength').value;
-        if (value > 0) {
-          randomPass(value);
-        }
-      });
-    });
-
-    if (checkNum == 0 || slider.value < 1) {
-      strengthTxt.textContent = "";
-      strengthBar1.style.backgroundColor = "#18171F";
-      strengthBar1.style.border = "2px solid #E6E5EA";
-      strengthBar2.style.backgroundColor = "#18171F";
-      strengthBar2.style.border = "2px solid #E6E5EA";
-      strengthBar3.style.backgroundColor = "#18171F";
-      strengthBar3.style.border = "2px solid #E6E5EA";
-      strengthBar4.style.backgroundColor = "#18171F";
-      strengthBar4.style.border = "2px solid #E6E5EA";
-    } else if (checkNum == 1 && value >= 1) {
-      strengthTxt.textContent = "TOO WEAK!";
-      strengthBar1.style.backgroundColor = "#F64A4A";
-      strengthBar1.style.border = "2px solid #F64A4A";
-      strengthBar2.style.backgroundColor = "#18171F";
-      strengthBar2.style.border = "2px solid #E6E5EA";
-      strengthBar3.style.backgroundColor = "#18171F";
-      strengthBar3.style.border = "2px solid #E6E5EA";
-      strengthBar4.style.backgroundColor = "#18171F";
-      strengthBar4.style.border = "2px solid #E6E5EA";
-    } else if (checkNum == 2 && value >= 2) {
-      strengthTxt.textContent = "WEAK";
-      strengthBar1.style.backgroundColor = "#FB7C58";
-      strengthBar1.style.border = "2px solid #FB7C58";
-      strengthBar2.style.backgroundColor = "#FB7C58";
-      strengthBar2.style.border = "2px solid #FB7C58";
-      strengthBar3.style.backgroundColor = "#18171F";
-      strengthBar3.style.border = "2px solid #E6E5EA";
-      strengthBar4.style.backgroundColor = "#18171F";
-      strengthBar4.style.border = "2px solid #E6E5EA";
-    } else if (checkNum == 3 && value >= 3) {
-      strengthTxt.textContent = "MEDIUM";
-      strengthBar1.style.backgroundColor = "#F8CD65";
-      strengthBar1.style.border = "2px solid #F8CD65";
-      strengthBar2.style.backgroundColor = "#F8CD65";
-      strengthBar2.style.border = "2px solid #F8CD65";
-      strengthBar3.style.backgroundColor = "#F8CD65";
-      strengthBar3.style.border = "2px solid #F8CD65";
-      strengthBar4.style.backgroundColor = "#18171F";
-      strengthBar4.style.border = "2px solid #E6E5EA";
-    } else if (checkNum == 4 && value >= 4) {
-      strengthTxt.textContent = "STRONG";
-      strengthBar1.style.backgroundColor = "#A4FFAF";
-      strengthBar1.style.border = "2px solid #A4FFAF";
-      strengthBar2.style.backgroundColor = "#A4FFAF";
-      strengthBar2.style.border = "2px solid #A4FFAF";
-      strengthBar3.style.backgroundColor = "#A4FFAF";
-      strengthBar3.style.border = "2px solid #A4FFAF";
-      strengthBar4.style.backgroundColor = "#A4FFAF";
-      strengthBar4.style.border = "2px solid #A4FFAF";
-    }
-    randomPass(value);
-  }
-  
-  function randomPass(sliderValue) {
-    let chars = "";
-    let pass = "";
-    check.forEach(function (box) {
-      if (box.checked && box.className == "checkUpper") {
-        chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      if (password.match(upperCase)) {
+          strength += 25;
       }
-      if (box.checked && box.className == "checkLower") {
-        chars += "abcdefghijklmnopqrstuvwxyz";
+      if (password.match(lowerCase)) {
+          strength += 25;
       }
-      if (box.checked && box.className == "checkNums") {
-        chars += "0123456789";
+      if (password.match(numbers)) {
+          strength += 25;
       }
-      if (box.checked && box.className == "checkSymbol") {
-        chars += "±!@#$%^&*()_+<>?|{}[];'/.,`~§";
+      if (password.match(symbols)) {
+          strength += 25;
       }
-    });
-    let charslength = chars.length;
-    for (let i = 0; i < sliderValue; i++) {
-      let random = Math.floor(Math.random() * charslength);
-      pass += chars.charAt(random);
-    }
-    password.textContent = pass;
-  }
+      if(length < 8){
+        strength -= 25;
+      }
+      if(length < 3){
+        strength -= 50;
+      }
+      return strength;
+      
+      }
+ 
+ 
+      function copied() {
+        copy.style.display = "flex";
+        navigator.clipboard.writeText(password.textContent);
+        setTimeout(function() {
+            copy.style.display = "none";
+          }, 2000);
+      }
